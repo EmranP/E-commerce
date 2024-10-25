4ab8facf845bf64a44d744d1bb4db036b08c526b
Emran <nightsking12345@gmail.com>
2024-10-25 15:38:34 +0500

    Start project and craete new repozetory

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import iconReact from '@assets/react.svg'
+
+export const App = () => {
+	return (
+		<>
+			<h1 className='text-2xl'>App e-commerce</h1>
+			<img src={iconReact} alt='' />
+		</>
+	)
+}
